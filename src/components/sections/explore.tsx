e6fcee92ba1ac4409961c53fa99ccbdc3d74f49c
rakib13x <rakib13x@gmail.com>
2025-04-01 08:30:01 +0600

    explore section complete

--- src/components/sections/explore.tsx
+++ src/components/sections/explore.tsx
@@ -1,15 +1,13 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import FoodData from "../../../public/json/food.json";
 import { useState } from "react";
 import { useGetAllCategories } from "@/hooks/category.hook";
 
 export default function Explore() {
-  const [search, setSearch] = useState("");
-  const [page, setPage] = useState(1);
+  const [search] = useState("");
+  const [page] = useState(1);
   const { data: CategoryData } = useGetAllCategories(search, page);
-  console.log(CategoryData);
 
   return (
     <section id="explore">
@@ -21,22 +19,22 @@ export default function Explore() {
           Our Delicious Menu
         </h2>
         <div className="grid grid-cols-1 gap-10 pt-10 lg:grid-cols-3">
-          {CategoryData?.data?.map((food, index) => (
+          {CategoryData?.data?.map((category) => (
             <Link
               href="/"
-              key={index}
+              key={category.categoryId}
               className="rounded-2xl border border-secondary-10 px-16 py-5 transition duration-300 hover:border-primary-100 hover:shadow-lg hover:shadow-[#050C11]/20"
             >
               <Image
-                src={`/assets/img/${CategoryData?.data}`}
+                src={category?.image || "/default-user.png"}
                 width={130}
                 height={130}
-                alt="Food Menu"
+                alt="category Menu"
                 className="mx-auto"
               />
-              <h5 className="text-heading-5 text-center">{food.category}</h5>
+              <h5 className="text-heading-5 text-center">{category.name}</h5>
               <h3 className="text-caption-2 text-center text-primary-100">
-                {food.items.length} Menu
+                {category.productCount} Menu
               </h3>
             </Link>
           ))}
